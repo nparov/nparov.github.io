30cc9848d4b80a8d4e17f5958b84255a9f20234f
sgoblin <rsuper@sonic.net>
2014-10-24 14:03:08 -0700

    Added team section

--- js/agency.js
+++ js/agency.js
@@ -24,3 +24,22 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+/*!
+ * NPA OpenROV custom scripts
+ */
+
+// Removes displayNone from the "clickOurNames"
+$("#clickNamesDiv").removeClass("displayNone");
+
+// Displays the team member's bio in the place of "click our names..."
+function putBioOnScreen(memberName){
+	if (memberName.getAttribute("id") == "ourAmazingTeam"){
+		$(".memberShowing").removeClass("memberShowing").addClass("displayNone");
+		$("#clickNamesBanner").removeClass("displayNone").addClass("memberShowing");
+	} else {
+		memberNameId = memberName.getAttribute("name");
+		$(".memberShowing").removeClass("memberShowing").addClass("displayNone");
+		$("#"+memberNameId).removeClass("displayNone").addClass("memberShowing");	
+	}
+}
